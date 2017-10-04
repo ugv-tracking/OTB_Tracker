c5d677f7e6b049fb99c867c749ec7e616c6c3bbe
maxtom <hitmaxtom@gmail.com>
2017-10-04 19:26:05 -0400

    ok

--- main_running.m
+++ main_running.m
@@ -182,7 +182,7 @@ for idxSeq=1:length(seqs)
     end
 end
 
-figure
+%figure
 t=clock;
 t=uint8(t(2:end));
 disp([num2str(t(1)) '/' num2str(t(2)) ' ' num2str(t(3)) ':' num2str(t(4)) ':' num2str(t(5))]);
